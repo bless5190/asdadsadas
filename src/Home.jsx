b9de3705e1df26de8d30e0650aec3a143056d45a
bless5190 <giovani.marth@gmail.com>
2025-05-01 23:32:38 -0300

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ color: 'white', padding: '2rem' }}>
+      <h1>CAST Serviços Digitais</h1>
+      <p>Seu site de compra e venda de criptoativos!</p>
+    </div>
+  );
+}
